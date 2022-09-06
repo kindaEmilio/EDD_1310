7389bf6b20fa3549008b4312a90e632ea11fc823
kindaEmilio <111693892+kindaEmilio@users.noreply.github.com>
2022-09-06 01:04:35 -0500

    Add files via upload

---
+++ salarois/src/Main.java
@@ -0,0 +1,10 @@
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Nomina nomina = new Nomina("junio.dat", "Estructura de datos S.A. de C.V.");
+        nomina.sueldo();
+        System.out.println("-----------------------------------------------------");
+        nomina.obtenerMaxMin();
+    }
+}
